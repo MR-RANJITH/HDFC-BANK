22fe77e355c10d1bdcb8a27be7f1bd3ee2230760
MR-RANJITH <91112022+MR-RANJITH@users.noreply.github.com>
2022-12-05 01:03:39 +0530

    COMPLETED

---
+++ frontend/public/home.js
@@ -0,0 +1,20 @@
+function Home(){
+  return (
+    <div class="Outline">
+    <div class="home">
+    <div class="">
+      <br/>
+      <p class="header">Welcome To,&nbsp;&nbsp;HDFC Bank</p>
+      <br/>
+      <p class="para">
+          HDFC Bank was incorporated in 1994 as a subsidiary of the Housing
+          Development Finance Corporation, with its registered office in Mumbai,
+          Maharashtra, India.
+      </p>
+      <br/>
+      <img src="bank.png" style={{ width: "700px" }}className="img-fluid" alt="Responsive image"/>
+    </div>
+    </div>
+    </div>
+  );  
+}
